c3b71f9af13ce400a5063bc0e6b55c6bbaf68b41
tu11aa <thanhtuhr01@gmail.com>
2022-07-07 22:53:51 +0700

    start creating routes

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
