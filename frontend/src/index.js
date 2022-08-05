ecdc443cb5cc70ac29e3d6f6e3ead5201e9c3a2f
madelineNguyen5702 <64205732+madelineNguyen5702@users.noreply.github.com>
2022-08-06 01:23:08 +0700

    Fix Landing Page v1

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,13 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./App";
-import "./index.css";
+
+
+// import ReactDOM from 'react-dom';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./flexboxgrid.min.css";
+import './index.css';
 
 const container = document.getElementById("root");
 const root = createRoot(container);
