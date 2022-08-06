1dba4d1f320931e6e24e741f64a8be38902be228
Thuan <thuanvo326@gmail.com>
2022-08-06 15:04:36 +0700

    add top home button

---
+++ frontend/src/components/Nav/HomeButton.jsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import { Link } from "react-scroll";
+import {useNavigate} from 'react-router-dom'
+// Assets
+import LogoIcon from "../../assets/svg/Logo";
+
+export default function HomeButton() {
+
+const navigate = useNavigate()
+
+  return (
+    <>
+        <NavInner className="container flexSpaceCenter">
+          <Link className="pointer flexNullCenter" to="home" smooth={true}>
+            <LogoIcon />
+            <h1 style={{ marginLeft: "15px" }} className="Abril Fatface" onClick={() => navigate('/')} >
+              Pomodoro
+            </h1>
+          </Link>
+          
+        </NavInner>
+    </>
+  );
+}
+
+
+const NavInner = styled.div`
+  position: relative;
+  height: 100%;
+`
+
+
+
+
